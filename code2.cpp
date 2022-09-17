ea31363d34046dc74a197a25b35c6735e40dc7e6
ItsWers0n <113796140+ItsWers0n@users.noreply.github.com>
2022-09-17 20:48:05 +0200

    Create code2.cpp

---
+++ code2.cpp
@@ -0,0 +1,73 @@
+int red_light_pin= 3;
+int green_light_pin = 4;
+int blue_light_pin = 5;
+int red_control = 6, blue_control = 7, green_control = 8, plus = 9, minus = 10;
+bool increase = true;
+
+
+void setup() {
+  RGB_color(0, 0, 0);
+  pinMode(red_light_pin, OUTPUT);
+  pinMode(green_light_pin, OUTPUT);
+  pinMode(blue_light_pin, OUTPUT);
+  pinMode(red_control, INPUT);
+  pinMode(green_control, INPUT);
+  pinMode(blue_control, INPUT);
+  pinMode(plus, INPUT);
+  pinMode(minus, INPUT);
+}
+void loop() {
+  RGB_control();
+  plus_Minus_Control();
+}
+void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
+ {
+  analogWrite(red_light_pin, red_light_value);
+  analogWrite(green_light_pin, green_light_value);
+  analogWrite(blue_light_pin, blue_light_value);
+}
+void RGB_control(int red = 0, int green = 0, int blue = 0)
+{
+  if(red >= 0 && red <= 255 && green >= 0 && green <= 255 && blue >= 0 && blue <= 255 &&)
+  {
+
+    if(analogRead(red_control) > 10 && increase == true)
+    {
+      red++;
+    }
+    if(analogRead(red_control) > 10 && increase == false)
+    {
+      red--;
+    }
+    if(analogRead(green_control) > 10 && increase == true)
+    {
+      green++;
+    }
+    if(analogRead(green_control) > 10 && increase == false)
+    {
+      green--;
+    }
+    if(analogRead(blue_control) > 10 && increase == true)
+    {
+      blue++;
+    }
+    if(analogRead(blue_control) > 10 && increase == false)
+    {
+      blue--;
+    }
+  }
+  RGB_color(red, green, blue);
+}
+
+void plus_Minus_Control()
+{
+  if(analogRead(plus) >= 10 && analogRead(minus) >= 10)
+  {
+  } else if(analogRead(plus) >= 10)
+  {
+    increase = true;
+  } else if(analogRead(minus) >= 10)
+  {
+    increase = false;
+  }
+}
